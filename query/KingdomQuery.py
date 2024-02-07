ecaaa1ffdec061770a48663f0bc6188ab6823f4b
anaamil <ana.amil@alumnos.upm.es>
2024-02-07 09:57:14 +0100

    Update KingdomQuery.py
    Búsqueda por patrón y diferenciación entre mayúsculas y minúsculas

--- query/KingdomQuery.py
+++ query/KingdomQuery.py
@@ -5,22 +5,43 @@ import re
 def is_isomeric(smiles):
     return any(c in smiles for c in ['\\', '/', '@'])
 
-def acceso_data(kingdom):
+def acceso_data_kingdom(kingdom):
     try:
         directory = glob("../processed_data/*/*_success.tsv")
         resultado=[]
         for file in directory:
             df = pd.read_csv(file, sep='\t', header=0, encoding='utf-8')
             if "classyfire.kingdom" in df.columns:
-                condition=df["classyfire.kingdom"]==kingdom
-                if not df[condition].empty and len(resultado)<10:
-                    resultado.append(df[condition])
+                query=df["classyfire.kingdom"].str.contains(kingdom, na=False)
+                if not df[query].empty and len(resultado)<10 and is_isomeric(df['smiles.std'].iloc[0]):
+                    resultado.append(df[query])
         if resultado:
             for i in resultado:
                 print(i)
+        else:
+            print(f'No se han encontrado coincidencias con {kingdom}')
+    except Exception as e:
+        print(f"Error:{e}")
+
+def acceso_data_superclass(superclass):
+    try:
+        directory = glob("../processed_data/*/*_success.tsv")
+        resultado=[]
+        for file in directory:
+            df = pd.read_csv(file, sep='\t', header=0, encoding='utf-8')
+            if "classyfire.superclass" in df.columns:
+                columna=df["classyfire.superclass"].str.lower()
+                query=columna.str.contains(superclass.lower(), na=False)
+                if not df[query].empty and len(resultado)<10 and is_isomeric(df['smiles.std'].iloc[0]):
+                    resultado.append(df[query])
+        if resultado:
+            for i in resultado:
+                print(i)
+        else:
+            print(f'No se han encontrado coincidencias con {superclass}')
     except Exception as e:
         print(f"Error:{e}")
 
 
-
-acceso_data("C44H84NO8P")
+#acceso_data_kingdom("Organic")
+#acceso_data_superclass("Nucleosides")
