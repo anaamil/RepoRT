162c87e157b3c7d401086b4bd33a1abfa19c9edb
anaamil <ana.amil@alumnos.upm.es>
2024-03-20 10:42:10 +0100

    Update ClassyFireQuery.py
    Concatenate chromatographic column dataframe with specific molecule rt data

--- query/ClassyFireQuery.py
+++ query/ClassyFireQuery.py
@@ -4,71 +4,61 @@ import sys
 from glob import glob
 import os
 import numpy as np
+import Gradient_data
 
 
 def is_isomeric(smiles):
+    """Return isomeric molecules
+
+    Keywords arguments:
+    smiles -- smiles molecule information
+    """
     return any(c in smiles for c in ['\\', '/', '@'])
 
 
-def access_metadata(file):
-    try:
-        met = pd.read_csv(file, sep='\t', header=0, encoding='utf-8')
-        return met
-    except Exception as e:
-        print(f"Error metadata:{e}")
-
-
-def access_gradient(file):
-    try:
-        gra = pd.read_csv(file, sep='\t', header=0, encoding='utf-8')
-        return gra
-    except Exception as e:
-        print(f"Error gradient data:{e}")
-
-
-def access_descriptors(file):
-    try:
-        des = pd.read_csv(file, sep='\t', header=0, encoding='utf-8')
-        return des
-    except Exception as e:
-        print(f"Error descriptors data:{e}")
-
-
 def access_data(pattern, location=".*"):
+    """Access to specific RepoRT data and return a complete dataframe
+
+    Keyword arguments:
+    pattern -- specific molecule type
+    location -- specific column of the dataframe (default * -- every column)
+    """
     try:
         directory = glob("../processed_data/*/*.tsv")
         results = []
-        info = []
+        alt = pd.read_csv("../../ReopRT_classified.tsv", sep='\t', header=0, encoding='utf-8')
         for file in directory:
-            if re.search(r"_metadata.tsv", file):
-                met = access_metadata(file)
-            if re.search(r"_gradient.tsv", file):
-                gra = access_gradient(file)
-            if re.search(r"_descriptors_isomeric_", file):
-                des = access_descriptors(file)
             df = pd.read_csv(file, sep='\t', header=0, encoding='utf-8')
             if "classyfire.kingdom" in df.columns and not is_isomeric(df['smiles.std'].iloc[0]):
                 column = df.filter(regex=f'{location}', axis=1)
                 column_string = column.select_dtypes(include=['object'])
                 for col in column_string.columns:
                     query = column[col].str.lower().str.contains(pattern.lower(), na=False)
-                    if not df[query].empty and len(results) < 5:
-                        df_concat = pd.merge(df[query], des, on=["id", "id"], how="left")
-                        data = [gra, met, df_concat]
-                        results.append(df_concat)
-                        info.append(data)
+                    if not df[query].empty:
+                        df_merge = df[query].merge(alt.drop(columns=[col for col in df[query].columns[1:]] + ["0"]), left_on="id", right_on="id",  how="left")
+                        results.append(df_merge)
                         break
         if column.size == 0:
             print(f"{location} not found")
         elif results:
-            for i in info:
-                print(f"\nColumn gradient:\n{i[0]} \nColumn data:\n{i[1]} \nMolecule data:\n {i[2]}")
-            # concat = pd.concat(results, axis=0)
-            # print(concat)
+            df_data = pd.concat(results, axis=0, ignore_index=True)
+            df_t_data = df_data.T
+            for col in df_t_data.columns:
+                df_t_data[col].iloc[df_t_data[col].duplicated()] = np.nan
+                val_null = df_t_data[col].iloc[14:][df_t_data[col].iloc[14:].isna()]
+                val_notnull = df_t_data[col].iloc[14:].dropna()
+                df_t_data[col].iloc[14:] = pd.concat([val_notnull, val_null], ignore_index=True)
+            df_data = df_t_data.T.replace("NA (NA)", np.nan).set_index("id")
+            df_data.to_csv("../../classyfire_data.tsv", sep="\t", index=True)
+            column_data = Gradient_data.gradient_data()
+            df_data["index"] = df_data.index.str[0:4].astype(int)
+            df = pd.merge(df_data, column_data, left_on="index", right_index=True, how="left")
+            df.drop(columns=["index"], inplace=True)
+            df.to_csv("../../final_data.tsv", sep='\t', index=True)
         else:
             print(f'No matches found with {pattern}')
     except Exception as e:
         print(f"Error:{e}")
 
 
-
+access_data("alcohols")
